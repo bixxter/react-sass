1f6c1e5c949d8159e5b54f26aef6f2bd6e26eb7a
Asatulla Dadabaev <bixxter@Asatullas-Air.bbrouter>
2021-07-29 18:42:45 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles/App.css";
+import union_logo from "./Union.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="card">
+        <div className="card__title">
+          <h1>synth</h1>
+          <span>
+            Design <br />
+            System
+          </span>
+        </div>
+        <div className="card__info">
+          <div className="card__info__description">
+            <ul>
+              <li>170+ Components</li>
+              <li>Variants</li>
+              <li>Icons</li>
+              <li>Autolayout Everything</li>
+            </ul>
+          </div>
+          <div className="card__info__logo">
+            <img src={union_logo} alt="card-logo" />
+            <span>Cred</span>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
